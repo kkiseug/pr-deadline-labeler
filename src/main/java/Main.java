59df869c95629497be0109c801b8e32e19b34fc8
GiSeok Kang <ime.rokz@gmail.com>
2026-03-01 23:14:32 +0900

    refactor: 액션 실패 시 로그 남기도록 변경

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,26 +11,35 @@ import java.time.format.DateTimeFormatter;
 public class Main {
 
     public static void main(String[] args) {
-        String token = System.getenv("INPUT_GITHUB-TOKEN");
-        String repo = System.getenv("GITHUB_REPOSITORY");
-        String baseDate = System.getenv("INPUT_BASE-DATE");
-        String prNumber = System.getenv("INPUT_PR-NUMBER");
-        String deadline = System.getenv("INPUT_DEADLINE-DAYS");
+        try {
+            String token = System.getenv("INPUT_GITHUB-TOKEN");
+            String repo = System.getenv("GITHUB_REPOSITORY");
+            String baseDate = System.getenv("INPUT_BASE-DATE");
+            String prNumber = System.getenv("INPUT_PR-NUMBER");
+            String deadline = System.getenv("INPUT_DEADLINE-DAYS");
 
-        HttpClient httpClient = HttpClient.newHttpClient();
-        LabelAttacher labelAttacher = new GithubLabelAttacher(httpClient, token, repo);
-        PullRequestRepository pullRequestRepository = new GithubPullRequestRepository(httpClient, token, repo);
-        LabelPolicy labelPolicy = new LabelPolicy(Long.parseLong(deadline));
-        LabelingService labelingService = new LabelingService(labelAttacher, pullRequestRepository, labelPolicy);
+            HttpClient httpClient = HttpClient.newHttpClient();
+            LabelAttacher labelAttacher = new GithubLabelAttacher(httpClient, token, repo);
+            PullRequestRepository pullRequestRepository = new GithubPullRequestRepository(httpClient, token, repo);
+            LabelPolicy labelPolicy = new LabelPolicy(Long.parseLong(deadline));
+            LabelingService labelingService = new LabelingService(labelAttacher, pullRequestRepository, labelPolicy);
 
-        LocalDate now = LocalDate.now();
-        if (prNumber == null) {
-            labelingService.attachLabels(now);
-        } else {
-            LocalDate realBaseDate = LocalDate.parse(baseDate, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+            LocalDate now = LocalDate.now();
+            if (prNumber == null) {
+                labelingService.attachLabels(now);
+            } else {
+                LocalDate realBaseDate = LocalDate.parse(baseDate,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
 
-            long prNum = Long.parseLong(prNumber);
-            labelingService.attachLabel(prNum, realBaseDate, now);
+                long prNum = Long.parseLong(prNumber);
+                labelingService.attachLabel(prNum, realBaseDate, now);
+            }
+        } catch (RuntimeException e) {
+            System.out.println("::error::" + e.getMessage());
+            if (e.getCause() != null) {
+                System.out.println("::error::caused by: " + e.getCause());
+            }
+            System.exit(1);
         }
     }
 }
